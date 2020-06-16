20572fb02079129c5db78fbe8dff3e708e54b34f
RomainPct <romain.penchenat@gmail.com>
2020-06-16 17:37:47 +0200

    Setup ThreeJS in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
-import { Header } from './parts/Header.js'
+import { Header } from './parts/Header'
 import { Footer } from './parts/Footer'
+import { Scene3D } from './parts/Scene3D'
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Scene3D/>
             <Footer/>
         </div>
         )
---
+++ src/parts/Scene3D.js
@@ -0,0 +1,53 @@
+import ReactDOM from 'react-dom'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+export function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export function Sphere(props) {
+    const mesh = useRef()
+    useFrame(() => (mesh.current.position.x += 0.01))
+    return (
+        <mesh
+            ref={mesh}
+            >
+            <sphereBufferGeometry attach="geometry" />
+            <meshStandardMaterial attach="material" color={'brown'} />
+        </mesh>
+    )
+}
+
+export function Scene3D() {
+    const mesh = useRef()
+    const [active, setActive] = useState(false)
+    return (
+        <Canvas style={{height:'100vh',width:'100vw'}}>
+            <ambientLight/>
+            <Sphere/>
+            <Box/>
+        </Canvas>
+    )
+}
