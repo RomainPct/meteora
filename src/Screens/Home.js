00e6da067956054ef0c72c48d6186a2327f048ec
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-17 16:54:09 +0200

    :sparkles: Home.js > Add text content

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { ButtonBar } from '../components/ButtonBar'
+import '../style/Home.css'
+import meteoraLogo from '../assets/images/logo.svg'
+import { GlobalContext } from '../contexts/GlobalContext'
 
 export const Home = () => {
+
+    const ctx = useContext(GlobalContext)
+
     return (
-        <main>
+        <main className="home-container">
             <h1>This is home</h1>
-            <Link to="/detailedMeteor/1">Meteor</Link>
-            <Link to="/detailedYear/2000">Year</Link>
-            <ButtonBar/>
+            {ctx.introductionIsDone ?
+                null
+                :
+                <div className="home-display-flex">
+                    <img className="meteora-logo-intro" src={meteoraLogo} alt="Meteora logo"/>
+                    <h3 className="home-catchphrase">All known meteors since the IXth century</h3>
+                </div>
+            }
         </main>
     )
 }
