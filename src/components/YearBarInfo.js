6ba0ac3d9a2ed5f411c69fc8608869dea9c84cf0
HugoLeroyHL <hugo.leroy@hetic.net>
2020-06-18 13:00:24 +0200

    :tada: Prepare data location for yearInfo

--- src/components/YearBarInfo.js
+++ src/components/YearBarInfo.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../style/YearBarInfo.css'
+import API from '../managers/API'
 
 export const YearBarInfo = (props) => {
-    
+
+    const [medianWeight, setMedianWeight] = useState(null)
+    const [biggestMeteor, setBiggestMeteor] = useState({})
+
+    useEffect(() => {
+        API.fetchMedianWeight(props.year, (response) => {
+            setMedianWeight(response)
+        })
+        API.fetchBiggestMeteor(props.year, (result) => {
+            setBiggestMeteor(result)
+        })
+    }, [])
+
+
+
     return (
         <div className="yearBarInfoContainer">
             <div className="Line"></div>
             <div className="yearBarInfoItems">
                 <div className="yearNameMenu">Year {props.year}</div>
-                <div className="yearBarMeteors">meteors</div>
-                <div className="yearBarAvMass">av. mass</div>
+                <div className="yearBarMeteors">{props.meteorsCount} meteors</div>
+                <div className="yearBarAvMass">{medianWeight} med. mass</div>
                 <div className="yearBarMassMin">mass min</div>
-                <div className="yearBarMassMax">highest mass</div>
+                <div className="yearBarMassMax">{biggestMeteor.mass} highest mass</div>
             </div>
         </div>
     )
