3d1c538147f79d17642f10f77e4748109b6f6076
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2020-06-18 10:57:04 +0200

    Page météor fixed

--- src/Screens/DetailedMeteor.js
+++ src/Screens/DetailedMeteor.js
@@ -7,41 +7,36 @@ export const DetailedMeteor = () => {
     return (
         <main>
             <h1>This is DetailedMeteor {id}</h1>
-            <Link to="/">Home</Link>
-            <Link to="/detailedYear/1990">Year</Link>
             <div className="detailedMeteorContainer">
                 <div className="detailedMeteorText">
-                    <h2>Fribolino</h2>
+                    <h3>Fribolino</h3>
                     <h3>1987</h3>
                     <h3>You might have seen it burn !</h3>
+                </div>
                     <div className="detailedMeteorUpperInfo">
                         <div className="meteorInfoCard">
                             <p>CLASS</p>
-                            <h2>L5</h2>
-                            <p class="infoDesc">Low-Iron meteor</p>
+                            <h3>L5</h3>
+                            <p className="infoDesc">Low-Iron meteor</p>
                         </div>
                         <div className="meteorInfoCard">
                             <p>MASS</p>
-                            <h2>246g</h2>
-                            <p class="infoDesc">As heavy as a smartphone ! 📱</p>
+                            <h3>346g</h3>
+                            <p className="infoDesc">As heavy as a smartphone ! 📱</p>
                         </div>
                     </div>
                     <div className="detailedMeteorLowerInfo">
                         <div className="meteorInfoCard">
                             <p>FALL LOCATION</p>
-                            <h2>Mexico 🇲🇽</h2>
-                            <p class="infoDesc">near Mexico city</p>
+                            <h3>Mexico 🇲🇽</h3>
+                            <p className="infoDesc">near Mexico city</p>
                         </div>
                         <div className="meteorInfoCard">
                             <p>DESTRUCTIVE POWER</p>
-                            <h2>80k Newtons</h2>
-                            <p class="infoDesc">The power of Niagara Falls 🌊</p>
+                            <h3>80k Newtons</h3>
+                            <p className="infoDesc">The power of Niagara Falls 🌊</p>
                         </div>
                     </div>
-
-                </div>
-
-
             </div>
         </main>
     )
