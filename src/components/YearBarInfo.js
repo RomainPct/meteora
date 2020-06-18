a71be25ba4eca7341002e8edcd2654f886281e20
HugoLeroyHL <hugo.leroy@hetic.net>
2020-06-18 15:03:40 +0200

    :tada: set API SmallestMeteor & integrate in YearInfo

--- src/components/YearBarInfo.js
+++ src/components/YearBarInfo.js
@@ -6,13 +6,17 @@ export const YearBarInfo = (props) => {
 
     const [medianWeight, setMedianWeight] = useState(null)
     const [biggestMeteor, setBiggestMeteor] = useState({})
+    const [smallestMeteor, setSmallestMeteor] = useState({})
 
     useEffect(() => {
         API.fetchMedianWeight(props.year, (response) => {
             setMedianWeight(response)
         })
-        API.fetchBiggestMeteor(props.year, (result) => {
-            setBiggestMeteor(result)
+        API.fetchBiggestMeteor(props.year, (response) => {
+            setBiggestMeteor(response)
+        })
+        API.fetchSmallestMeteor(props.year, (response) => {
+            setSmallestMeteor(response)
         })
     }, [])
 
@@ -25,7 +29,7 @@ export const YearBarInfo = (props) => {
                 <div className="yearNameMenu">Year {props.year}</div>
                 <div className="yearBarMeteors">{props.meteorsCount} meteors</div>
                 <div className="yearBarAvMass">{medianWeight} med. mass</div>
-                <div className="yearBarMassMin">mass min</div>
+                <div className="yearBarMassMin">{smallestMeteor.mass} mass min</div>
                 <div className="yearBarMassMax">{biggestMeteor.mass} highest mass</div>
             </div>
         </div>
