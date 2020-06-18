65af82ddffcde9e86a716ee0e83e83dfc412a1e5
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2020-06-18 12:13:17 +0200

    adaptative valors detailed meteor

--- src/Screens/DetailedMeteor.js
+++ src/Screens/DetailedMeteor.js
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../style/DetailedMeteor.css'
 import { Link, useParams } from 'react-router-dom'
+import API from '../managers/API'
+
 
 export const DetailedMeteor = () => {
     const { id } = useParams()
+
+
+    const [detailedMeteor, setDetailedMeteor] = useState({})
+    useEffect(() => {
+        API.fetchMeteor(id, (result) => {
+            setDetailedMeteor(result)
+        })
+    }, [])
+
     return (
         <main>
-            <h1>This is DetailedMeteor {id}</h1>
+            <h1>This is DetailedMeteor {detailedMeteor.id}</h1>
             <div className="detailedMeteorContainer">
                 <div className="detailedMeteorText">
-                    <h3>Fribolino</h3>
+                    <h3> {detailedMeteor.name} </h3>
                     <h3>1987</h3>
                     <h3>You might have seen it burn !</h3>
                 </div>
                     <div className="detailedMeteorUpperInfo">
                         <div className="meteorInfoCard">
                             <p>CLASS</p>
-                            <h3>L5</h3>
+                            <h3>{detailedMeteor.recclass}</h3>
                             <p className="infoDesc">Low-Iron meteor</p>
                         </div>
                         <div className="meteorInfoCard">
                             <p>MASS</p>
-                            <h3>346g</h3>
+                            <h3>{detailedMeteor.mass}g</h3>
                             <p className="infoDesc">As heavy as a smartphone ! 📱</p>
                         </div>
                     </div>
