fc028f8ba7b7ee619fb56897e866c4ea3d2618ec
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2020-06-19 16:38:29 +0200

    🍾WORKING graph

--- src/parts/detailedYear/MassDistributionGraph.js
+++ src/parts/detailedYear/MassDistributionGraph.js
@@ -1,83 +1,90 @@
 import React from 'react'
 import BubbleChart from '@weknow/react-bubble-chart-d3';
+import { LogLuvEncoding } from 'three';
 
 export const MassDistributionGraph = (props) => {
 
     function sortMeteorsByMass() {
-        const from1gto50g = []
-        const from50gto100g = []
-        const from100gto150g = []
-        const from150gto200g = []
-        const from200gto250g = []
-        const from250g500g = []
-        const from500gto750g = []
-        const from750gto1kg = []
-        const from1kgto1and5kg = []
-        const from1and5kgto2kg = []
-        const from2kgto2and5kg = []
-        const from2and5kgto3kg = []
-        const from3kgto3and5kg = []
-        const from3and5kgto4kg = []
-        const from4kg = []
+        const categories = [
+            { label: '1g-50g', value: 0 },
+            { label: '50g-100g', value: 0 },
+            { label: '100g-150g', value: 0 },
+            { label: '150g-200g', value: 0 },
+            { label: '200g-250g', value: 0 },
+            { label: '250g-500g', value: 0 },
+            { label: '500g-750g', value: 0 },
+            { label: '750g-1kg', value: 0 },
+            { label: '1kg-1.5kg', value: 0 },
+            { label: '1.5kg-2kg', value: 0 },
+            { label: '2kg-2.5kg', value: 0 },
+            { label: '2.5kg-3kg', value: 0 },
+            { label: '3kg-3.5kg', value: 0 },
+            { label: '3.5kg-4kg', value: 0 },
+            { label: '4kg+', value: 0 }]
 
-        console.log(props.meteors);
 
         props.meteors.forEach(meteor => {
 
-            if (meteor>0 && meteor<51) {
-                from1gto50g.push(meteor)
+            if (meteor.mass < 51) {
+                categories[0].value += 1
             }
-            if (meteor>50 && meteor<101) {
-                from50gto100g.push(meteor)
+            if (meteor.mass>50 && meteor.mass<101) {
+                categories[1].value += 1
             }
-            if (meteor>100 && meteor<151) {
-                from100gto150g.push(meteor)
+            if (meteor.mass>100 && meteor.mass<151) {
+                categories[2].value += 1
             }
-            if (meteor>150 && meteor<201) {
-                from150gto200g.push(meteor)
+            if (meteor.mass>150 && meteor.mass<201) {
+                categories[3].value += 1
             }
-            if (meteor>200 && meteor<251) {
-                from200gto250g.push(meteor)
+            if (meteor.mass>200 && meteor.mass<251) {
+                categories[4].value += 1
             }
-            if (meteor>250 && meteor<501) {
-                from250g500g.push(meteor)
+            if (meteor.mass>250 && meteor.mass<501) {
+                categories[5].value += 1
             }
-            if (meteor>500 && meteor<751) {
-                from500gto750g.push(meteor)
+            if (meteor.mass>500 && meteor.mass<751) {
+                categories[6].value += 1
             }
-            if (meteor>750&& meteor<1001) {
-                from750gto1kg.push(meteor)
+            if (meteor.mass>750&& meteor.mass<1001) {
+                categories[7].value += 1
             }
-            if (meteor>1000&& meteor<1501) {
-                from1kgto1and5kg.push(meteor)
+            if (meteor.mass>1000&& meteor.mass<1501) {
+                categories[8].value += 1
             }
-            if (meteor>1500&& meteor<2001) {
-                from1and5kgto2kg.push(meteor)
+            if (meteor.mass>1500&& meteor.mass<2001) {
+                categories[9].value += 1
             }
-            if (meteor>2000&& meteor<2501) {
-                from2kgto2and5kg.push(meteor)
+            if (meteor.mass>2000&& meteor.mass<2501) {
+                categories[10].value += 1
             }
-            if (meteor>2500&& meteor<3001) {
-                from2and5kgto3kg.push(meteor)
+            if (meteor.mass>2500&& meteor.mass<3001) {
+                categories[11].value += 1
             }
-            if (meteor>3000&& meteor<3501) {
-                from3kgto3and5kg.push(meteor)
+            if (meteor.mass>3000&& meteor.mass<3501) {
+                categories[12].value += 1
             }
-            if (meteor>3500&& meteor<4001) {
-                from3and5kgto4kg.push(meteor)
+            if (meteor.mass>3500&& meteor.mass<4001) {
+                categories[13].value += 1
             }
-            if (meteor>4000) {
-                from4kg.push(meteor)
+            if (meteor.mass>4000) {
+                categories[14].value += 1
             }
+            
+
         });
         
+        console.log(categories);
         
-
+        
+        return categories
+        
 
     }
 
-
-    sortMeteorsByMass()
+    
+    
+    
 
 
     return (
@@ -89,8 +96,8 @@ export const MassDistributionGraph = (props) => {
     offsetX: -0.05,
     offsetY: -0.01,
   }}
-  width={1000}
-  height={800}
+  width={800}
+  height={600}
   padding={0} // optional value, number that set the padding between bubbles
   showLegend={true} // optional value, pass false to disable the legend.
   legendPercentage={20} // number that represent the % of with that legend going to use.
@@ -114,24 +121,7 @@ export const MassDistributionGraph = (props) => {
       }}
   //Custom bubble/legend click functions such as searching using the label, redirecting to other page
 
-  data={[
-    { label: 'CRM', value: 1 },
-    { label: 'API', value: 1 },
-    { label: 'Data', value: 1 },
-    { label: 'Commerce', value: 1 },
-    { label: 'AI', value: 3 },
-    { label: 'Management', value: 5 },
-    { label: 'Testing', value: 6 },
-    { label: 'Mobile', value: 9 },
-    { label: 'Conversion', value: 9 },
-    { label: 'Misc', value: 21 },
-    { label: 'Databases', value: 22 },
-    { label: 'DevOps', value: 22 },
-    { label: 'Javascript', value: 23 },
-    { label: 'Languages / Frameworks', value: 25 },
-    { label: 'Front End', value: 26 },
-    { label: 'Content', value: 26 },
-  ]}
+  data={sortMeteorsByMass()}
 />
         </section>
     )
