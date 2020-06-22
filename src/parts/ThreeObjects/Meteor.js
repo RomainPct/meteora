fa1cb646078373778ce8d4b3336e3d6f8a36b415
RomainPct <romain.penchenat@gmail.com>
2020-06-22 18:54:36 +0200

    ☄️Add meteor textures

--- src/parts/ThreeObjects/Meteor.js
+++ src/parts/ThreeObjects/Meteor.js
@@ -1,17 +1,57 @@
-import React, { useState, useRef } from 'react'
-import { useFrame } from 'react-three-fiber'
-import { MeshPhongMaterial } from 'three'
+import React, { useState, useRef, Suspense, useMemo } from 'react'
 import { animated } from 'react-spring/three'
+import { useFrame, useLoader } from 'react-three-fiber'
+import { TextureLoader, MeshPhongMaterial, Vector2 } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+import meteorTexture from '../../assets/meteor/texture.jpg'
+import meteorNormalMap from '../../assets/meteor/normal.png'
+import meteorSpecularMap from '../../assets/meteor/specular.png'
+
+function MeteorFallback(props) {
+    return (
+        <mesh
+            material={new MeshPhongMaterial({
+                color: 0xaaaaaa
+            })}
+            >
+            <sphereBufferGeometry attach="geometry" args={props.size} />
+        </mesh>
+    )
+}
+
+function MeteorModel(props) {
+
+    const mesh = useRef()
+    const texture = useMemo(() => new TextureLoader().load(meteorTexture), [])
+    const normalMap = useMemo(() => new TextureLoader().load(meteorNormalMap), [])
+    const specularMap = useMemo(() => new TextureLoader().load(meteorSpecularMap), [])
+    const { nodes } = useLoader(GLTFLoader, '/3D_objects/meteor.glb');
+
+    useFrame(() => (mesh.current.rotation.z = mesh.current.rotation.z += 0.01))
+
+    return (
+        <mesh
+            ref={mesh}
+            geometry={nodes.meteor.geometry}
+            scale={[0.1, 0.1, 0.1]}
+            material={new MeshPhongMaterial({
+                map: texture,
+                normalMap: normalMap,
+                normalScale: new Vector2( 10, 10 ),
+                specularMap: specularMap
+            })}
+            >
+        </mesh>
+    )
+}
 
 export function Meteor(props) {
     // This reference will give us direct access to the mesh
-    const mesh = useRef()
+    // const mesh = useRef()
     
     // Set up state for the hovered and active state
     const [hovered, setHover] = useState(false)
-
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
     function size(isCapsule = false) {
         const size = Math.max(Math.min(props.meteor.mass / 4000, 0.2), 0.01) + (isCapsule ? 0.02 : 0)
@@ -24,14 +64,9 @@ export function Meteor(props) {
             onPointerOver={_ => setHover(true)}
             onPointerOut={_ => setHover(false)}
             >
-            <mesh
-                ref={mesh}
-                material={new MeshPhongMaterial({
-                    color: 0xaaaaaa
-                })}
-                >
-                <sphereBufferGeometry attach="geometry" args={size()} />
-            </mesh>
+            <Suspense fallback={<MeteorFallback size={size()} />}>
+                <MeteorModel />
+            </Suspense>
             {hovered ?
                 <mesh
                     material={new MeshPhongMaterial({
