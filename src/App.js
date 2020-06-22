cafcc999bc1527f59b4d3325311b48708879c49e
RomainPct <romain.penchenat@gmail.com>
2020-06-22 15:21:05 +0200

    :stars: First version of transition between pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './style/App.css'
-import { Header } from './parts/Header'
-import { Scene3D } from './parts/Scene3D'
-import { Home } from './Screens/Home'
-import { DetailedMeteor } from './Screens/DetailedMeteor'
-import { DetailedYear } from './Screens/DetailedYear'
 import { GlobalContext, defaultGlobalContext } from './contexts/GlobalContext'
 import {
     BrowserRouter as Router,
-    Switch,
-    Route
   } from 'react-router-dom'
 import API from './managers/API'
+
+import { AppContent } from './parts/AppContent'
 
 function App() {
 
@@ -102,19 +97,7 @@ function App() {
     return ( 
         <GlobalContext.Provider value={ctx}>
             <Router>
-                <Header/>
-                <Scene3D/>
-                <Switch>
-                    <Route path="/detailedMeteor/:id">
-                        <DetailedMeteor/>
-                    </Route>
-                    <Route path="/detailedYear/:yearIndex">
-                        <DetailedYear/>
-                    </Route>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
+                <AppContent/>
             </Router>
         </GlobalContext.Provider>
     )
---
+++ src/parts/AppContent.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Header } from './Header'
+import { Scene3D } from './Scene3D'
+import { Home } from '../Screens/Home'
+import { DetailedMeteor } from '../Screens/DetailedMeteor'
+import { DetailedYear } from '../Screens/DetailedYear'
+import {
+    Switch,
+    Route,
+    useLocation
+  } from 'react-router-dom'
+
+import {useTransition, animated} from 'react-spring'
+
+export const AppContent = () => {
+
+    const location = useLocation()
+
+    const pageTransition = useTransition(
+        location,
+        location => location.pathname,
+        {
+            from: { opacity: 0 },
+            enter: { opacity: 1 },
+            leave: { opacity: 0 },
+            config: { duration: 500 }
+        }
+    )
+
+    return (
+        <div>
+            <Header/>
+            <Scene3D/>
+            {pageTransition.map(({item, key, props}) => (
+                <animated.div key={key} style={props} className='mainContainer' >
+                    <Switch location={item} >
+                        <Route path="/detailedMeteor/:id">
+                            <DetailedMeteor/>
+                        </Route>
+                        <Route path="/detailedYear/:yearIndex">
+                            <DetailedYear/>
+                        </Route>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </animated.div>
+            ))}
+        </div>
+    )
+}
