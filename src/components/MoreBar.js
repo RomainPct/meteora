13c69587400db4495d322d7c6dedf9c402981c8d
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2020-06-24 11:50:16 +0200

    added sound button

--- src/components/MoreBar.js
+++ src/components/MoreBar.js
@@ -1,23 +1,30 @@
 import '../style/MoreBar.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {animated, useSpring} from 'react-spring'
+import soundOn from '../assets/images/volumeup.svg'
+import soundOff from '../assets/images/volumeoff.svg'
 
 export const MoreBar = () => {
 
     const location = useLocation()
-
     const aboutUsSpring = useSpring({
         opacity: location.pathname !== '/aboutUs' ? 1 : 0,
         transform: location.pathname !== '/aboutUs' ? 'translateY(0%)' : 'translateY(-50%)',
         config: { duration: 400 }
     })
-
+    const changeSound = ()=>{
+        setIsPlaying(
+            !isPlaying
+        )
+    }
+    const [isPlaying, setIsPlaying] = useState(false)
     return (
-        <div>
+        <div className="moreBarContainer">
             <animated.div className="aboutUsButton" style={aboutUsSpring}>
                 <Link to="/aboutUs">About Us</Link>
             </animated.div>
+            <img src={isPlaying ? soundOn : soundOff} className="soundButton" onClick={changeSound}/>
         </div>
     )
 
